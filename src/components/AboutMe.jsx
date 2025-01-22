75a3a60a351c732b3fe946632d27223c8793790e
jutalexa2024 <jajustinalexander@gmail.com>
2025-01-22 11:03:19 -0600

    update

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import profilePicture from '../assets/avatar.png';
 
 const AboutMe = () => {
   return (
+    
     <div className="min-h-screen w-screen bg-gray-100 flex items-center justify-center">
+      <img src={profilePicture} alt="Avatar" className="w-36 h-36 rounded-full mx-auto" />
       <div className="w-full max-w-8xl mx-6 p-8 bg-white rounded-xl shadow-lg">
-        <p className="text-5xl text-gray-800 leading-relaxed">
+        <p className="text-3xl text-gray-800 leading-relaxed">
           Sociable on as carriage my position weddings raillery consider. Peculiar
           trifling absolute and wandered vicinity property yet. The and collecting
           motionless difficulty son. His hearing staying ten colonel met. Sex drew
